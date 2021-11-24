10cdbd2f4f3d5004f84ff5ed8701096bdfcacd4c
GeorgeEGO <geo.eliseev@gmail.com>
2021-11-24 08:58:44 +0500

    fill array

--- Program.cs
+++ Program.cs
@@ -2,5 +2,11 @@
 
 
 // See https://aka.ms/new-console-template for more information
-int[] arr = Array.Empty<int>();
+int[] arr = new int[5];
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = i + 1;
+}
+
+Console.WriteLine(string.Join(' ', arr));
 Console.WriteLine("Hello, World! " + arr.Length);
